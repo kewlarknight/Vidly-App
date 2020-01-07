3dc7b5041039cf6912b2eab9944d4d342031eda0
kewlarknight <kewal.zaware@gmail.com>
2020-01-07 09:28:56 +0530

    update

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const winston = require("winston");
 const express = require("express");
 const app = express();
+
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-require('./startup/prod')(app); 
+require('./startup/prod')(app);
 
 //Server
-const port=process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, (req, res) => {
   winston.info(`Listening on port ${port}.....`);
 });
